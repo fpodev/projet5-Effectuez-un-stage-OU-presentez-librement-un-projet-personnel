b81f36d378cf40fd83076d2dcd9479e48c40745c
fpodev <fpo@gmx.fr>
2020-03-15 11:31:21 +0100

    test

--- Librairies/Objet/ConnexionDb.php
+++ Librairies/Objet/ConnexionDb.php
@@ -4,17 +4,12 @@ namespace App\Objet;
 use PDO;
 
 class ConnexionDb
-{
-    private $host = 'localhost';
-    private $dbName = 'GMAO';
-    private $user = 'fabrice';
-    private $pwd = 'Frbrl7C90848467';    
-
+{              
     public static function getPDO()
-    {
-        $db = new PDO("mysql:host=$host;dbname=$dbName; charset=utf8", $user , $pwd);
-        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
-
-        return $db;       
+    {            
+        $db = new PDO('mysql:host=localhost;dbname=GMAO;charset=utf8','fabrice', 'Frbrl7C90848467');
+        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);       
+    
+        return $db;
     }
 }
