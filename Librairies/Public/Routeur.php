881f89ce6224f4bca78eea4347fe704995133551
fpodev <fpo@gmx.fr>
2020-03-15 11:31:55 +0100

    construction

--- Librairies/Public/Routeur.php
+++ Librairies/Public/Routeur.php
@@ -1,7 +1,22 @@
 <?php
+
+use App\Controller\UserControler;
 require 'vendor/autoload.php';
 
-$loader = new \Twig\Loader\FilesystemLoader('Librairies/templates');
+/*$loader = new \Twig\Loader\FilesystemLoader('Librairies/templates');
 $twig = new \Twig\Environment($loader, [
     'cache' => 'Librairies/compilation_cache',
-]);
+]);*/
+
+class routeur{
+
+         private $ctrlUser;
+
+    public function __construct(){
+        $this->ctrlUser = new UserControler();
+    }
+    public function connect()
+    {
+        include ('Librairies/View/CreateUser.php');
+    }
+}
