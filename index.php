31cd5c1a4d25a2df0db168c19c4edef41779904b
fpodev <fpo@gmx.fr>
2020-03-15 11:30:46 +0100

    modif pour passage routeur

--- index.php
+++ index.php
@@ -1,16 +1,6 @@
-<html>
-<body id="loginPage">        
- <p>page de connexion</p>
-    <div class='login'>
-        <form action="index.php?login" method="post">
-            <p>
-            <label for="identifiant">Identifiant:</label></br>
-            <input type="text" name='identifiant'/></br>
-            <label for="pass">Mot de passe:</label></br>
-            <input type="password" name="pass" /></br>
-            <input type="submit" name="connexion" value="connexion" />
-            </p>
-        </form>
-    </div>             
-</body>
-</html> 
+<?php
+
+require 'Librairies/Public/Routeur.php';
+
+$routeur = new Routeur();
+$routeur->connect();
