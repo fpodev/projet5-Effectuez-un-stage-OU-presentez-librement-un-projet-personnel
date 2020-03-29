04cf05724d41575e6d13ebce2a7bf82fa25a8040
fpodev <fpo@gmx.fr>
2020-03-29 18:09:35 +0200

    Modification passage router

--- index.php
+++ index.php
@@ -1,6 +1,17 @@
 <?php
+use App\Routeur\Routeur;
+require 'vendor/autoload.php';
+session_start();
+          
+$router = new Routeur($_GET['url']);
+$router->get('/', "User#home"); 
+$router->get('/Accueil', 'User#home');
+$router->get('/Modifier=:id', "User#changeUser");
+$router->get('/Liste-Utilisateurs', "User#listUser");
+$router->get('/Supprimer=:id', "User#deleteUser");
+$router->get('/Ajouter-Utilisateur', "User#UserPage");
+$router->post('/Accueil', "User#connexion"); 
+$router->post('/Liste', "User#addUser");
 
-require 'Librairies/Public/Routeur.php';
 
-$routeur = new Routeur();
-$routeur->connect();
+$router->run(); 
