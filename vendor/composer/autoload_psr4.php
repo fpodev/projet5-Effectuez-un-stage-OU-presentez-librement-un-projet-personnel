78b0148ce19c4d91d54a60746712eea0319a9162
fpodev <fpo@gmx.fr>
2020-03-29 18:20:23 +0200

    ajout

--- vendor/composer/autoload_psr4.php
+++ vendor/composer/autoload_psr4.php
@@ -9,5 +9,6 @@ return array(
     'Twig\\' => array($vendorDir . '/twig/twig/src'),
     'Symfony\\Polyfill\\Mbstring\\' => array($vendorDir . '/symfony/polyfill-mbstring'),
     'Symfony\\Polyfill\\Ctype\\' => array($vendorDir . '/symfony/polyfill-ctype'),
+    'Symfony\\Component\\VarDumper\\' => array($vendorDir . '/symfony/var-dumper'),
     'App\\' => array($baseDir . '/Librairies'),
 );
