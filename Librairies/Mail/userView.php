f57eaa88fe57bc607bd0d5ad8d1174c8543b17be
fpodev <fpo@gmx.fr>
2020-04-13 18:39:20 +0200

    Creation

---
+++ Librairies/Mail/userView.php
@@ -0,0 +1,14 @@
+
+<html>  
+<body>
+<?php
+echo "<p>Bonjour " .$prenom. "</p></br>
+      <p>Voici votre identifiant et votre mot de passe pour vous connecter à l'application GMAO.</p></br>
+      <p>Identifiant:  ".$destinataire." </p></br>
+      <p>Mot de passe: ".$pass." </p></br></br>
+      <p>Nous vous rappellons qu'il est important de changer votre mot de passe réguliérement</p></b></br>
+      <p>Cordialement.</p> </br>
+      <p> Votre administrateur GMAO </p>"
+?>
+</body>
+</html>
