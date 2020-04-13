dea523a61578a5a35a5d234d28aba484dc30a470
fpodev <fpo@gmx.fr>
2020-04-13 18:59:30 +0200

    ok

--- Librairies/Objet/ConnexionDb.php
+++ Librairies/Objet/ConnexionDb.php
@@ -7,7 +7,7 @@ class ConnexionDb
 {              
     public static function getPDO()
     {            
-        $db = new PDO('mysql:host=localhost;dbname=GMAO;charset=utf8',');
+        $db = new PDO('mysql:host=localhost;dbname=GMAO;charset=utf8','fabrice', 'Frbrl7C90848467');
         $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);       
     
         return $db;
