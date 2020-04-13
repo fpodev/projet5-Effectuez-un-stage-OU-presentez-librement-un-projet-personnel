1b4b4638295cf253c3553de8b51f6e27a0221de7
fpodev <fpo@gmx.fr>
2020-04-13 18:37:37 +0200

    Ajout DocHeader + modif de noms

--- index.php
+++ index.php
@@ -1,17 +1,28 @@
 <?php
+/*
+Author: fpodev (fpodev@gmx.fr)
+index.php (c) 2020
+Desc: description
+Created:  2020-04-13T09:42:15.632Z
+Modified: !date!
+*/
+
 use App\Routeur\Routeur;
-require 'vendor/autoload.php';
-session_start();
-          
+require 'vendor/autoload.php'; 
+
 $router = new Routeur($_GET['url']);
+
 $router->get('/', "User#home"); 
 $router->get('/Accueil', 'User#home');
+$router->post('/Accueil', "User#connexion"); 
+
 $router->get('/Modifier=:id', "User#changeUser");
-$router->get('/Liste-Utilisateurs', "User#listUser");
+$router->get('/Gestion-Utilisateurs', "User#listUser");
 $router->get('/Supprimer=:id', "User#deleteUser");
 $router->get('/Ajouter-Utilisateur', "User#UserPage");
-$router->post('/Accueil', "User#connexion"); 
-$router->post('/Liste', "User#addUser");
+$router->post('/Liste-Utilisateurs', "User#addUser");
 
+$router->get('/Gestion-Lieux', "Site#siteListe");
+$router->get('Ajouter-Site','Site#sitePage');
 
 $router->run(); 
