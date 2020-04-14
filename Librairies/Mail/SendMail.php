2a585c1d7f74ac36d04d288101e66b910135d979
fpodev <fpo@gmx.fr>
2020-04-14 08:24:58 +0200

    Update SendMail.php

--- Librairies/Mail/SendMail.php
+++ Librairies/Mail/SendMail.php
@@ -31,8 +31,8 @@ class SendMail{
         $this->mail->isSMTP();                                            // Send using SMTP
         $this->mail->Host       = 'mail.gmx.com ';                    // Set the SMTP server to send through
         $this->mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-        $this->mail->Username   = 'fabrice.pourinet@gmx.fr';                     // SMTP username
-        $this->mail->Password   = 'vA1yK19VqPrd';                               // SMTP password
+        $this->mail->Username   = '*****.fr';                     // SMTP username
+        $this->mail->Password   = ****;                               // SMTP password
         $this->mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPthis->maile::rENCRYPTION_SMTPS` encouraged
         $this->mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPthis->mailer::ENCRYPTION_SMTPS` above
 
