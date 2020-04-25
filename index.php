c4f6570c4bed9d1056253972aebdf77c294ab216
fpodev <fpo@gmx.fr>
2020-04-25 09:35:56 +0200

    Ajout de nouvelle route

--- index.php
+++ index.php
@@ -6,23 +6,38 @@ Desc: description
 Created:  2020-04-13T09:42:15.632Z
 Modified: !date!
 */
-
 use App\Routeur\Routeur;
 require 'vendor/autoload.php'; 
 
 $router = new Routeur($_GET['url']);
 
 $router->get('/', "User#home"); 
-$router->get('/Accueil', 'User#home');
-$router->post('/Accueil', "User#connexion"); 
+$router->get('Accueil', 'User#home');
+$router->post('Accueil', "User#connexion"); 
 
-$router->get('/Modifier=:id', "User#changeUser");
-$router->get('/Gestion-Utilisateurs', "User#listUser");
-$router->get('/Supprimer=:id', "User#deleteUser");
-$router->get('/Ajouter-Utilisateur', "User#UserPage");
-$router->post('/Liste-Utilisateurs', "User#addUser");
+$router->get('Modifier=:id', "User#changeUser");
+$router->get('Gestion-Utilisateurs', "User#listUser");
+$router->get('Supprimer=:id', "User#deleteUser");
+$router->get('Ajouter-Utilisateur', "User#UserPage");
+$router->post('Liste-Utilisateurs', "User#addUser");
 
-$router->get('/Gestion-Lieux', "Site#siteListe");
-$router->get('Ajouter-Site','Site#sitePage');
+$router->get('Gestion-Lieux', "Lieu#LieuList");
+$router->get('Ajouter-Site','Lieu#LieuPage');
+
+$router->get('Villes=:id', 'Batiment#batimentList');
+$router->get('Gestion-Batiments', 'Batiment#batimentList');
+
+$router->get('Bâtiments=:id', 'Secteur#secteurList');
+$router->get('Secteurs=:id', 'Materiel#materielList');
+
+$router->get('Demande-Travaux', 'Travaux#travauxPage');
+$router->get('Batiment=:id', 'Travaux#travauxPage');
+$router->get('Secteur=:id','Travaux#travauxPage');
+$router->get('Matériel=:id', 'Travaux#formulaireTravaux');
+$router->post('Validation', 'Travaux#addTravaux');
+$router->get('Liste-nouveaux-travaux', 'Travaux#TravauxList');
+$router->get('Planifier-travaux=:id', 'Travaux#uniqueTravaux');
+$router->post('Planification-valide', 'Travaux#addTravaux');
+
 
 $router->run(); 
