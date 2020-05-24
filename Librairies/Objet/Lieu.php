563830ef689cfa6fccf26c606e965673b182eae7
fpodev <fpo@gmx.fr>
2020-05-24 17:01:56 +0200

    ajout descriptif

--- Librairies/Objet/Lieu.php
+++ Librairies/Objet/Lieu.php
@@ -1,12 +1,18 @@
 <?php
 namespace App\Objet;
-
+/*
+Author: fpodev (fpodev@gmx.fr)
+Lieu.php (c) 2020
+Desc: Objet lieu n'ayant qu'une seule valeur, les contrôles ce font dans l'ExtendsObjet.
+Created:  2020-04-13T14:03:28.788Z
+Modified: !date!
+*/
 use App\Objet\ExtendsObjet;
 class Lieu extends ExtendsObjet
 {             
     public function isValid()
     {
-        return !(empty($this->name));
+        return !(empty($this->nom));
     }    
 }
 
