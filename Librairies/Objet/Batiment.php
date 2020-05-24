22df64b95877d0472aa1cd99018d101c69e252b4
fpodev <fpo@gmx.fr>
2020-05-24 17:00:58 +0200

    Modif du fonction isValid et ajout descriptif

--- Librairies/Objet/Batiment.php
+++ Librairies/Objet/Batiment.php
@@ -1,17 +1,25 @@
 <?php
 namespace App\Objet;
-
+/*
+Author: fpodev (fpodev@gmx.fr)
+Batiment.php (c) 2020
+Desc: Objet batiment.
+Created:  2020-04-13T14:03:28.788Z
+Modified: !date!
+*/
 use App\Objet\ExtendsObjet;
 class Batiment extends ExtendsObjet
 {    
     
-    private $id_lieu;   
-
-    const NAME_INVALIDE = 1;
+    private $id_lieu;     
  
     public function isValid()
     {
-        return !(empty($this->name) || empty($this->id_lieu));
+        return !(empty($this->nom) || empty($this->id_lieu));
+    }
+    public function isValidUpdate()
+    {
+        return !(empty($this->nom));
     }
     //setter 
     public function setId_lieu($id_lieu){
