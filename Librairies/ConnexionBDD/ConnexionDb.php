7693ebc4adf45447bbfb8b2906253bdb7677254a
fpodev <fpo@gmx.fr>
2020-05-30 09:27:07 +0200

    Ajout erreur

--- Librairies/ConnexionBDD/ConnexionDb.php
+++ Librairies/ConnexionBDD/ConnexionDb.php
@@ -7,15 +7,21 @@ Desc: Gére la connexion à la base de donnée
 Created:  2020-04-13T14:03:28.788Z
 Modified: !date!
 */
+
+use Exception;
 use PDO;
 
 class ConnexionDb
 {              
     public static function getPDO()
-    {            
-        $db = new PDO('mysql:host=localhost;dbname=GMAO;charset=utf8','fabrice', 'Frbrl7C90848467');
-        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);       
-    
+    {   
+        try{
+            $db = new PDO('mysql:host=localhost;dbname=GMAO;charset=utf8','fabrice', 'Frbrl7C90848467');
+            $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);              
+        }catch(Exception $e){
+            echo $e -> getmessage();
+        }       
+        
         return $db;
     }
 }
