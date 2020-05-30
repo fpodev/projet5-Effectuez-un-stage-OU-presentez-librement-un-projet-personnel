d3a9a3adcdba1ea5984634d6d506dabdf2890233
fpodev <fpo@gmx.fr>
2020-05-30 10:22:58 +0200

    Modif condition dans listUser

--- Librairies/model/UserModel.php
+++ Librairies/model/UserModel.php
@@ -21,14 +21,15 @@ class UserModel
     }
     public function add(User $user)
     {
-        $q = $this->db->prepare('INSERT INTO User (nom, prenom , email, pwd, id_lieu, niveau , userAdd, userModif) VALUES (:nom, :prenom, :email, :pwd, :id_lieu, :niveau, :userAdd, :userModif)');
+        $q = $this->db->prepare('INSERT INTO User (nom, prenom , email, pwd, id_lieu, niveau , userAdd, userModif) 
+                                VALUES (:nom, :prenom, :email, :pwd, :id_lieu, :niveau, :userAdd, :userModif)');
     
         $q->bindValue(':nom', $user->nom(), PDO::PARAM_STR);
         $q->bindValue(':prenom', $user->prenom(), PDO::PARAM_STR);
         $q->bindValue(':email', $user->email(), PDO::PARAM_STR);
         $q->bindValue(':pwd', $user->pwd(), PDO::PARAM_STR);
         $q->bindValue(':id_lieu', $user->lieu(), PDO::PARAM_INT);
-        $q->bindValue(':niveau', $user->niveau(), PDO::PARAM_STR);
+        $q->bindValue(':niveau', $user->niveau(), PDO::PARAM_INT);
         $q->bindValue(':userAdd', $user->userAdd(), PDO::PARAM_STR);
         $q->bindValue(':userModif', $user->userModif(), PDO::PARAM_STR);
 
@@ -39,10 +40,20 @@ class UserModel
         $this->db->exec('DELETE FROM User WHERE id= '.(int)$id);
     }
     public function listUser()
-    {
-   
-        $q = $this->db->query('SELECT User.id, User.nom, prenom, email, niveau, userAdd, Lieu.nom AS "lieu" FROM User INNER JOIN Lieu ON User.id_lieu = Lieu.id ORDER BY id DESC');
-
+    {   
+        if($_SESSION['niveau'] == 0){
+        $q = $this->db->prepare('SELECT User.id, User.nom, prenom, email, niveau, userAdd, Lieu.nom AS "lieu" FROM User 
+                                INNER JOIN Lieu ON User.id_lieu = Lieu.id ORDER BY id DESC ');
+        }
+        else{//niveau 1
+            $q = $this->db->prepare('SELECT User.id, User.nom, prenom, email, niveau, userAdd, Lieu.nom AS "lieu" FROM User 
+                                    INNER JOIN Lieu ON User.id_lieu = Lieu.id WHERE User.id_Lieu = :id_lieu 
+                                    AND NOT User.niveau = :niveau ORDER BY id DESC ');
+             
+       }             
+         $q->bindValue(':niveau', "0",PDO::PARAM_INT);
+         $q->bindValue(':id_lieu', $_SESSION['lieuId'],PDO::PARAM_INT);
+         
         $q->execute();
 
         $q->setFetchMode(PDO::FETCH_ASSOC);
@@ -54,8 +65,7 @@ class UserModel
         return $userList;
     }    
     public function listTech($id)
-     {      
-        
+     {              
         $q = $this->db->prepare('SELECT nom, prenom, id FROM User WHERE id_lieu = :id AND niveau = "2" ');
 
         $q->bindParam(':id',$id , PDO::PARAM_INT);
@@ -110,23 +120,26 @@ class UserModel
             throw new RuntimeException('L\'utilisateur doit être valide pour être enregistré');
         }
     }  
+
     public function connexion($identifiant)
     {
         $q = $this->db->prepare('SELECT * FROM User WHERE email = :identifiant');        
        
-        $q->bindValue(':identifiant', $identifiant);                
+        $q->bindValue(':identifiant', $identifiant);              
 
-        $q->execute();  
+        $q->execute(); 
         
+        $q->setFetchMode(PDO::FETCH_CLASS | PDO::FETCH_PROPS_LATE, 'App\Objet\User');
+
         $resultat = $q->fetch();
 
         $q->closeCursor();
 
         return $resultat;         
     }    
-    public function nouveauPass($identifiant){
+    public function nouveauPass($identifiant, $passNew){
         
-        $pass_hache = password_hash($_POST['newPass'], PASSWORD_DEFAULT);
+        $pass_hache = password_hash($passNew, PASSWORD_DEFAULT);
          
         $q = $this->db->prepare('UPDATE User SET pwd = :pass WHERE email = :identifiant');       
         
